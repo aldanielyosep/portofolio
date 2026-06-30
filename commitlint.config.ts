7141aaacac19b68cadd3b3c308f8d4230b4bd2ef
aldanielyosep <aldanielyosep@gmail.com>
2026-06-30 20:45:24 +0700

    feat: add commitlint rules

---
+++ commitlint.config.ts
@@ -0,0 +1,30 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'deps',
+        'release',
+        'security',
+      ],
+    ],
+
+    'subject-case': [0],
+
+    'header-max-length': [2, 'always', 100],
+  },
+}
